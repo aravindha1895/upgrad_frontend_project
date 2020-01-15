92f97c65005e01cd39517d3047d46b340e30a0a8
aravindha1895 <sridharanrvnd@gmail.com>
2020-01-15 21:30:39 +0530

    Update IP
    Update IP

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://18.212.74.66:8080/api/v1/";
+    this.baseUrl = "http://ec2-52-23-254-81.compute-1.amazonaws.com:8080/api/v1/";
   }
   render(){
     return(
